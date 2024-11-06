e8e096a3bc58c97a838726af76a6cc58be61e611
omarremila <84892442+omarremila@users.noreply.github.com>
2024-11-06 16:43:13 -0500

    Update LightForm.tsx

--- src/components/LightForm.tsx
+++ src/components/LightForm.tsx
@@ -3,7 +3,7 @@ import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { Sun } from 'lucide-react';
 import { defineConfig, loadEnv } from 'vite';
 
-const BACKEND_URL = process. import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL =  import.meta.env.VITE_BACKEND_URL;
 const GOOGLE_MAPS_API_KEY =  import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 const mapContainerStyle = {
